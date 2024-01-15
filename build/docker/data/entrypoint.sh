4422eb8353ca798e3a5218b3be8a4d6dcbb84e5e
02ggang9 <02ggang9@gmail.com>
2024-01-15 13:41:48 +0900

    fix: db url 수정

--- build/docker/data/entrypoint.sh
+++ build/docker/data/entrypoint.sh
@@ -6,4 +6,4 @@ cd /home/doo-re
 
 gradle build --daemon --build-cache --parallel
 
-java -jar -Duser.timezone=Asia/Seoul /home/doo-re/build/libs/springbatch-0.0.1-SNAPSHOT.jar
+java -jar -Duser.timezone=Asia/Seoul /home/doo-re/build/libs/spring-batch-0.0.1-SNAPSHOT.jar
